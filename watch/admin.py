1f5da4252e83c7083b1863ab9a456f9a8766543d
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-21 23:40:15 +0300

    Add the base.html

--- watch/admin.py
+++ watch/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from watch.models import Business, Category, Location, Neighborhood, Post
 
 # Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Business)
