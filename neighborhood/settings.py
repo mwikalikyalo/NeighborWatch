e5df04ea3965378e603f800104ad5f931fbafa9d
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-22 23:06:22 +0300

    Add final adjustments

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -102,10 +102,13 @@ WSGI_APPLICATION = 'neighborhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'neighborwatch',
+            'USER': config('DB_USER', default=''),
+            'PASSWORD': config('DB_PASSWORD', default=''),
+        },
     }
-}
+
 
 
 # Password validation
--- watch/forms.py
+++ watch/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from django_registration.forms import RegistrationForm
 from .models import Business, Post, Profile
 from django.contrib.auth.models import User
 
-class CreateUserForm(forms.ModelForm):
+class CreateUserForm(RegistrationForm):
   class Meta:
     model = User
     fields = ('username', 'email',)
