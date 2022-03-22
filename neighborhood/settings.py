55cd9252c6b55a3faa44758352bc8384056989c7
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-22 13:30:44 +0300

    Add email configurations

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -15,6 +15,7 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,7 +62,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'watch',
     'bootstrap5',
-    'cloudinary'
+    'cloudinary',
+    'registration'
 ]
 
 MIDDLEWARE = [
@@ -139,8 +141,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
