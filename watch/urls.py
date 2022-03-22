413d74cadeb7e75b76b3abf56d7e552c79490232
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-22 10:18:14 +0300

    Add admin urls

---
+++ watch/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+  path('index/', views.index, name='home'),
+  path('', views.profile, name='profile'),
+  path('search/', views.search, name='search'),
+  path('ajax/search/', views.searchajax, name='searchajax')
+]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
