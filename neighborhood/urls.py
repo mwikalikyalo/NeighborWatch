5d722d304770eadc1db67d485abd9abc4fddbc66
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-22 21:28:12 +0300

    Add the new migrations

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('watch.urls')),
-    path('accounts/', include('django_registration.backends.activation.urls')),
+    # path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
