279ce809287abd8d1b9e9cb9e6e9e9852b78bc49
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-03-22 08:22:36 +0300

    Add the post.html template

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'watch'
+    'watch',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
